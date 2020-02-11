5dfe792983b94bbdfa98880c985ffb7acef10e00
nighteagle82 <nighteagle82xd@hotmail.com>
2020-02-11 12:06:13 +0800

    试下IP

--- auth/src/main/java/cn/ne/aopauth/controller/TestController.java
+++ auth/src/main/java/cn/ne/aopauth/controller/TestController.java
@@ -11,6 +11,7 @@ import com.alibaba.fastjson.JSON;
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -66,5 +67,40 @@ public class TestController {
         return JSON.toJSONString(resMap);
     }
 
+    @RequestMapping(value = "/ip", method = RequestMethod.GET, produces = {"application/json;charset=UTF-8"})
+    public String testRequest(HttpServletRequest request) {
+        Map<String, Object> resMap = new HashMap<>();
 
+        resMap.put("request.getLocalAddr()", request.getLocalAddr());
+        resMap.put("request.getRemoteAddr()", request.getRemoteAddr());
+        resMap.put("request.getRemoteHost()", request.getRemoteHost());
+
+        System.out.println(
+                "request.getLocalAddr()===> " + request.getLocalAddr() + "\n" +
+                        "request.getRemoteAddr()===> " + request.getRemoteAddr() + "\n" +
+                        "request.getRemoteAddr()===> " + request.getRemoteHost() + "\n"
+        );
+        System.out.println("ip ==> " + getIpAddress(request));
+        return JSON.toJSONString(resMap);
+    }
+
+    public static String getIpAddress(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
 }
